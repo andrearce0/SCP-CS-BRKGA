eb339f95d7a30a56a9da4dd39d01d1c4530f0d6b
André Arce <andrearce343@gmail.com>
2025-11-06 18:30:55 -0300

    alteracao em assinatura de funcao
    adicionado parametro opcional na funcao decodificar

--- decodificador.hpp
+++ decodificador.hpp
@@ -3,10 +3,8 @@
 
 #include "scp_cs_data.hpp"
 #include <vector>
-#include <utility>
 
-double decodificar(std::vector<float> genes, const SCPCSInstance& instancia);
+double decodificar(std::vector<float> genes, const SCPCSInstance& instancia, std::set<int>* solucao_saida = nullptr);
 std::set<int> busca_local_remocao(std::set<int> solucao_inicial, SCPCSInstance& instancia, double& custo_inicial);
 double calcular_custo_solucao(const std::set<int>& subconjuntos_selecionados, const SCPCSInstance& instancia);
-std::set<int> decodificar_para_solucao(std::vector<float> genes, const SCPCSInstance& instancia);
 #endif // DECODIFICADOR_HPP
